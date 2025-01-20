da34d182168820f75eb1b33b924ec54fa3563785
David Cormell <d.cormell@etoncollege.org.uk>
2025-01-20 15:32:03 +0000

    End of video on complex types using ForEach

--- RubberDucks/ContentView.swift
+++ RubberDucks/ContentView.swift
@@ -8,12 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rubberDucks: [RubberDuck] = [
+       RubberDuck(
+           id: UUID(),
+           name: "Captain Quack",
+           colour: "Yellow",
+           size: "Large",
+           theme: "Pirate"
+       ),
+       RubberDuck(
+           id: UUID(),
+           name: "Doctor Ducky",
+           colour: "White",
+           size: "Medium",
+           theme: "Medical"
+       ),
+       RubberDuck(
+           id: UUID(),
+           name: "Princess Paddleton",
+           colour: "Pink",
+           size: "Small",
+           theme: "Royal"
+       ),
+       RubberDuck(
+           id: UUID(),
+           name: "Agent Waddles",
+           colour: "Black",
+           size: "Medium",
+           theme: "Spy"
+       ),
+       RubberDuck(
+           id: UUID(),
+           name: "Chef Quackington",
+           colour: "Orange",
+           size: "Large",
+           theme: "Culinary"
+       ),
+       RubberDuck(
+           id: UUID(),
+           name: "Chef Quackington",
+           colour: "Blue",
+           size: "Small",
+           theme: "Culinary"
+       )
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(rubberDucks) { rubberDuck in
+                HStack {
+                    Text("Name: \(rubberDuck.name)")
+                    Text("Size: \(rubberDuck.size)")
+                }
+                Divider()
+            }
         }
         .padding()
     }
---
+++ RubberDucks/RubberDuck.swift
@@ -0,0 +1,16 @@
+//
+//  Duck.swift
+//  RubberDucks
+//
+//  Created by Cormell, David - DPC on 20/01/2025.
+//
+
+import Foundation
+
+struct RubberDuck: Identifiable {
+    let id: UUID
+    let name: String
+    let colour: String
+    let size: String
+    let theme: String
+}
