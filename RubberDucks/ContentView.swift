820afae8cbfd42b371463c7a6c9d002a6fedbd11
David Cormell <d.cormell@etoncollege.org.uk>
2025-01-20 14:46:33 +0000

    Initial Commit

---
+++ RubberDucks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RubberDucks
+//
+//  Created by Cormell, David - DPC on 20/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RubberDucks/RubberDucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  RubberDucksApp.swift
+//  RubberDucks
+//
+//  Created by Cormell, David - DPC on 20/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct RubberDucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
